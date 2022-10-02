b515f262ba2eb14fa78fdeaab0febfd4e86abe32
Simplyvoda <efem.vodina@gmail.com>
2022-10-02 14:59:32 +0100

    setup webstatic

---
+++ 0-setup_web_static.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+# this bash script sets up the web server for deployment of web_static
+
+#-- install nginx
+sudo apt-get update
+sudo apt-get -y install nginx
+sudo ufw allow 'Nginx HTTPS'
+
+#-- creating folders
+sudo mkdir /data/ /data/web_static/ /data/web_static/releases/ /data/web_static/releases/test/ /data/web_static/shared/
+
+#-- HTML page with content
+sudo echo -e '<h1>Welcome to my page!</h1>\n<p>currently building vodinaefem.tech</p>' > /data/web_static/releases/test/index.html
+
+#-- create symbolic link (check if exists and deletes to recreate)
+sudo ln -sf /data/web_static/releases/test/ /data/web_static/current
+
+#-- change ownership of /data/ folder
+sudo chown -R ubuntu:ubuntu /data/
+
+#-- update nginx configuration
+sudo chown 755 /etc/nginx/sites-available/default
+sudo sed -i '/server_name _/a\		location /hbnb_static/ {\n\t\talias /data/web_static/current/;\n\t}\n' /etc/nginx/sites-available/default
+sudo ln -sf '/etc/nginx/sites-available/default' '/etc/nginx/sites-enabled/default'
+
+sudo service nginx restart
