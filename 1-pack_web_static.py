a8e0e1c2bcf6e7918c83dc286ab725434fbd1428
Vodina Efem <efem.vodina@gmail.com>
2022-10-02 20:43:27 +0000

    pep8 fix

--- 1-pack_web_static.py
+++ 1-pack_web_static.py
@@ -5,13 +5,14 @@ fabric script that generates .tgz archive from web_static contents
 from fabric.api import *
 import time
 
+
 def do_pack():
     """function to generate tgz archive"""
     timestamp = time.strftime("%Y%m%d%H%M%S")
     try:
         local("mkdir -p versions")
-        local("tar -cvzf versions/web_static_{:s}.tgz web_static/".
-                format(timestamp))
+        local("tar -cvzf versions/web_static_{:s}.tgz web_static/"
+              .format(timestamp))
         return ("versions/web_static_{:s}.tgz".format(timestamp))
     except FileNotFoundError:
         return None
