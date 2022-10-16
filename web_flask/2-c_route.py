d8be798a8a6a0205a8666e52bbd4082dc56f1684
Simplyvoda <efem.vodina@gmail.com>
2022-10-16 15:55:09 +0100

    commit

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """
 Module contains script to start flask web application
-Routes "/" - returns Hello HBNB!
+Routes :
+"/" - returns Hello HBNB!
 "/hbnb" - returns HBNB
+"/c/<text>" - returns C followed by value of text variable
 """
 from flask import Flask
 app = Flask(__name__)
@@ -13,10 +15,17 @@ def hello():
     """Function that returns Hello HBNB!"""
     return "Hello HBNB!"
 
+
 @app.route("/hbnb", strict_slashes=False)
 def hbnb():
     """Function that returns HBNB"""
     return "HBNB"
+
+@app.route("/c/<text>", strict_slashes=False)
+def c_text(text):
+    """Function that displays C and text variable"""
+    text = text.replace("_", " ")
+    return "C {}".format(text)
 
 
 if __name__ == '__main__':
