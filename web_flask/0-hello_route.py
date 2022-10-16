95df86976b458e7cfac49313e0eca105f230a67f
Vodina Efem <efem.vodina@gmail.com>
2022-10-16 13:52:51 +0000

    commit

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """
 Module contains script to start flask web application
+Routes "/" - returns Hello HBNB!
 """
 from flask import Flask
 app = Flask(__name__)
 
-@app.route('/', strict_slashes=False)
+
+@app.route("/", strict_slashes=False)
 def hello():
     """Function that returns Hello HBNB!"""
     return "Hello HBNB!"
 
+
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=5000, debug=True)
+    app.run(host="0.0.0.0", port=5000)
